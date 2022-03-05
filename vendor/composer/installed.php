6366b55f60560dac6fb910e9831d24053e4863a4
Dante <dante03918@gmail.com>
2022-03-05 17:17:16 +0100

    second

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '08a560449eeea31fddcf1e34a08edc0f436fe17b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '08a560449eeea31fddcf1e34a08edc0f436fe17b',
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v212',
+            'version' => '212.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => '8b90c86b7bd15f6a516bb2d8e877cb30cd77b90a',
+            'dev_requirement' => true,
+        ),
+    ),
+);
